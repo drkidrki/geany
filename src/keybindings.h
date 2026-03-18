c11e9db3ebdefc271b0370a4e9f6c0089c921ba3
Drki <darkmart@gmail.com>
2026-03-18 13:09:40 +0100

    Add keybinding to collapse project files tree

--- src/keybindings.h
+++ src/keybindings.h
@@ -280,6 +280,7 @@ enum GeanyKeyBindingID
 												 * @since 1.38 (API 240) */
 	GEANY_KEYS_PROJECT_NEW_FROM_FOLDER,			/**< Keybinding.
 												 * @since 2.0 (API 243) */
+	GEANY_KEYS_PROJECT_COLLAPSE_FILES_TREE,		/**< Keybinding. */
 	GEANY_KEYS_COUNT	/* must not be used by plugins */
 };
 

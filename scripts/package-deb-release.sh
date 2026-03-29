44c80dcb1df5e21db92095c5349458429ea3e3bf
Drki <darkmart@gmail.com>
2026-03-29 17:34:01 +0200

    .deb package fixes

--- scripts/package-deb-release.sh
+++ scripts/package-deb-release.sh
@@ -29,11 +29,13 @@ rm -rf "$PKGROOT"
 #mkdir -p "$PKGROOT/usr/share/geany"
 #mkdir -p "$PKGROOT/usr/share/icons/hicolor/48x48/apps"
 
-cp -r "$PREFIX/usr" "$PKGROOT/usr"
-
-# create additional directories
+# Create directory structure
 mkdir -p "$DEBIAN_DIR"
+mkdir -p "$PKGROOT/usr/
 mkdir -p "$PKGROOT/usr/share/applications"
+
+# copy all installed files
+cp -r "$PREFIX/usr" "$PKGROOT/usr"
 
 # Copy program files
 #echo "Copying files..."
